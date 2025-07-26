df5ca9a6a7a30540aaf9269ff83a69f8e7fe3c63
jaswanth Sai <jasvanthsai7@gmail.com>
2025-07-26 19:49:43 +0530

    added origins

--- backend-qai/query-ai.API/Program.cs
+++ backend-qai/query-ai.API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:5173", "https://query-ai-812b.onrender.com/", "https://vercel.com/")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
